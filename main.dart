a2e15b55c2bb4f231ab0dd8d254f33a5dd07054a
QUINTEROJ0 <144061707+QUINTEROJ0@users.noreply.github.com>
2024-03-22 13:53:35 -0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SearchPage() , 
+    );
+  }
+}
+  class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => SearchPageState();
+}
+
+class SearchPageState extends State<SearchPage> {
+
+  static List< 
+
+
+void updateList(String value){
+
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1f1545),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1f1545),
+        elevation: 0.0,
+      ) ,
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Search a Movie",
+               style:TextStyle(
+                color: Colors.white, 
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0, 
+            ),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xff302360),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: "eg: The Dark Knight",
+                prefixIcon: Icon(Icons.search),
+                prefixIconColor: Colors.purple.shade900,
+              ),
+            ),
+            SizedBox(height: 20.0 ,),
+            Expanded(
+              child: ListView(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
